ff86c30f043539481fe6e1b5aa14132d4170aa44
Anneke Kleppe <anneke@warmerkleppe.nl>
2025-02-01 15:37:23 +0100

    Open model dialog finished - apart from actually opening the model

--- packages/webapp/src/lib/app-parts/OpenModelDialog.svelte
+++ packages/webapp/src/lib/app-parts/OpenModelDialog.svelte
@@ -1,20 +1,94 @@
 <script lang="ts">
-	import { Button, Modal, Label, Input, Radio } from 'flowbite-svelte';
+	import { Button, Modal, Label, Input, Radio, Helper } from 'flowbite-svelte';
 	import { dialogs } from '$lib/stores/Dialogs.svelte';
 	import { serverInfo } from '$lib/stores/ModelInfo.svelte';
+	import { setUserMessage } from '$lib';
+	import * as Keys from "@freon4dsl/core"
 
+	const initialHelperText: string = "Enter or select a name.";
+	let helperText: string = initialHelperText;
+	const invalidHelperText: string = "Name may contain only characters and numbers, and must start with a character.";
+	let internalSelected: string = ""; // used for radio buttons
 	let newName: string = '';
-	let internalSelected: string = ''
+	$: newName = internalSelected.length > 0 ? internalSelected : '';
+	let nameInvalid: string;
+	$: nameInvalid = newName.length > 0 ? newNameInvalid() : '';
+
+	function newNameInvalid(): string {
+		if (newName === internalSelected) {
+			return ''; // one of the existing models is selected, this is ok => not invalid
+		} else {
+			if (!newName.match(/^[a-z,A-Z][a-z,A-Z0-9_]*$/)) {
+				helperText = invalidHelperText;
+				return 'invalid';
+			} else {
+				return '';
+			}
+		}
+	}
+
+	function resetVariables() {
+		dialogs.openModelDialogVisible = false;
+		serverInfo.allModelNames = [];
+		newName = "";
+		internalSelected = "";
+		helperText = initialHelperText;
+	}
+
+	function handleCancel() {
+		// if ($initializing) {
+			setUserMessage("You must select or create a model, before you can start!");
+		// }
+		dialogs.openModelDialogVisible = false;
+		resetVariables();
+	}
+
+	function handleSubmit() {
+		alert('Handle "success": ' + newName)
+		resetVariables();
+	}
+
+	function isKeyBoardEvent(event: Event): event is KeyboardEvent {
+		return 'detail' in event;
+	}
+
+	const handleEnterKey = (event: Event) => {
+		console.log("handleEnterKey " + event)
+		if (isKeyBoardEvent(event)) {
+			switch (event.key) {
+				case Keys.ENTER: { // on Enter key try to submit
+					event.stopPropagation();
+					event.preventDefault();
+					if (!newNameInvalid()) {
+						handleSubmit();
+						resetVariables();
+					}
+					break;
+				}
+			}
+		}
+	}
+
 </script>
 
-<Modal bind:open={dialogs.openModelDialogVisible} size="xs" autoclose={false} class="w-full">
-	<div class="flex flex-col space-y-6" >
+<Modal bind:open={dialogs.openModelDialogVisible} size="xs" autoclose={false} class="w-full" >
+
+	<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
+	<div class="flex flex-col space-y-6" on:keydown={handleEnterKey} role="dialog">
+
 		<h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Open Model</h3>
 		<Label class="space-y-2">
 			<span>Name of model</span>
-			<Input bind:value={newName} type="text" name="model-name" placeholder="my_model" required />
+			<Input bind:value={newName}
+						 type="text"
+						 name="model-name"
+						 placeholder="my_model"
+						 required
+			/>
+			<Helper class="mt-2 text-primary-900" >
+				<span class="font-medium">{helperText}</span>
+			</Helper>
 		</Label>
-
 		<div class="grid grid-cols-4">
 			{#each serverInfo.allModelNames as model}
 				<Radio class="p-2" name="models" on:change={() => {internalSelected = model}} >{model}</Radio>
@@ -22,9 +96,10 @@
 		</div>
 
 	</div>
+
 	<svelte:fragment slot="footer">
-		<Button on:click={() => alert('Handle "success": ' + internalSelected)}>Open</Button>
-		<Button color="alternative" on:click={() => alert('Handle "cancel": ' + internalSelected)}>Cancel</Button>
+		<Button on:click={handleSubmit}>Open</Button>
+		<Button color="alternative" on:click={handleCancel}>Cancel</Button>
 	</svelte:fragment>
 
 </Modal>
