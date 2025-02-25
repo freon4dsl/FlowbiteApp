8d671c341ddd983954b50aed789d8cb8bca3e532
Anneke Kleppe <anneke@warmerkleppe.nl>
2025-02-25 11:54:52 +0100

    Changes in dsl to fit the use in another webapp

--- packages/dsl/src/loggers.ts
+++ packages/dsl/src/loggers.ts
@@ -9,4 +9,6 @@ export function configureLoggers() {
     // FreLogger.unmute("FreLionwebSerializer")
     // // Do not show log messages containing any of these strings
     // FreLogger.setFilter(["beforeUpdate", "setFocus", "afterUpdate", "onMount", "REFRESH", "refresh"])
+	// FreLogger.unmuteAllLogs();
+	// FreLogger.unmute("FreonComponent")
 }
